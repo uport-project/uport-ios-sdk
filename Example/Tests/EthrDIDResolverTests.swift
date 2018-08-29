ba9a8a3acb8b8475ed788aa34977c1c8582c7078
Joshua Bell <belljh@gmail.com>
2018-08-29 10:40:00 -0400

    work on normalizing did and unit tests around that

---
+++ Example/Tests/EthrDIDResolverTests.swift
@@ -0,0 +1,49 @@
+//
+//  EthrDIDResolverTests.swift
+//  uPortSDK_Tests
+//
+//  Created by mac on 8/27/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+
+import Quick
+import Nimble
+@testable import uPortSDK
+
+class EthrDIDResolverTests: QuickSpec {
+    override func spec() {
+        describe("EthrDIDResolver") {
+            it( "can normalize DID" ) {
+                let validDids = [
+                    "0xb9c5714089478a327f09197987f16f9e5d936e8a",
+                    "0xB9C5714089478a327F09197987f16f9E5d936E8a",
+                    "did:ethr:0xb9c5714089478a327f09197987f16f9e5d936e8a",
+                    "did:ethr:0xB9C5714089478a327F09197987f16f9E5d936E8a"
+                ]
+                
+                let invalidDids = [
+                    "0xb9c5714089478a327f09197987f16f9e5d936e",
+                    "B9C5714089478a327F09197987f16f9E5d936E8a",
+                    "ethr:0xb9c5714089478a327f09197987f16f9e5d936e8a",
+                    "B9C5714089478a327F09197987f16f9E5d936E8a",
+                    "B9C5714089478a327F09197987f16f9E5d936E"
+                ]
+                
+//                let mockJsonRPC = JsonRPC()
+//                let mockResolver = EthrDIDResolver( rpc: mockJsonRPC)
+                for validDid in validDids {
+                    let normalizedDid = NormalizedDID( didCandidate: validDid )
+                    expect( !normalizedDid.value.isEmpty ).to( beTrue() )
+                    expect( normalizedDid.value.lowercased() ) == "did:ethr:0xb9c5714089478a327f09197987f16f9e5d936e8a"
+                }
+                
+                for invalidDid in invalidDids {
+                    let normalizedDid = NormalizedDID( didCandidate: invalidDid )
+                    print( "normalizeddid is " + normalizedDid.value )
+                    expect( normalizedDid.value.isEmpty ).to( beTrue() )
+                }
+            }
+        }
+    }
+}
--- uPortSDK/Classes/EthrDID/EthrDIDResolver.swift
+++ uPortSDK/Classes/EthrDID/EthrDIDResolver.swift
@@ -12,27 +12,39 @@ public struct EthrDIDResolver {
     var registryAddress = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b"
     var rpc : JsonRPC
 
-    public init( rpc: JsonRPC, registeryAddress: String = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b" ) {
+    public init( rpc: JsonRPC, registryAddress: String = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b" ) {
         self.rpc = rpc
         self.registryAddress = registryAddress
     }
 
     func resolve( did: String ) -> DDO {
-        var normalizeDid = normalizeDid( did )
-
+        let normalizedDid = NormalizedDID( didCandidate: did  )
+        let identity = self.parseIdentity( normalizedDid: normalizedDid.value )
+        let ethrdidContract = EthrDID(address: identity, rpc: self.rpc, registry: self.registryAddress )
+//        let owner = ethrdidContract.lookupOWner( cache: false )
+        return DDO( id: "TODO: implement" )
     }
-
-    private let didParsePattern = try? NSRegularExpression( pattern: "^(did:)?((\\w+):)?((0x)([0-9a-fA-F]{40}))", options: .caseInsensitive )
-    private func normalizeDid( did: String ) -> String {
-        let matchedSubstrings = didParsePattern?.matches( in: did, options: [], range: NSRange(location: 0, length: did.count) ).map { (result: NSTextCheckingResult) -> String in
-            return did.substring(with: result.range)
-        }
-
-        guard matchedSubstrings != nil && 0 < matchedSubstrings.count else {
+    
+    public let identityExtractionPattern = try? NSRegularExpression( pattern: "^did:ethr:(0x[0-9a-fA-F]{40})", options: .caseInsensitive )
+    
+    func parseIdentity( normalizedDid: String ) -> String {
+        let textCheckingResults = self.identityExtractionPattern?.matches(in: normalizedDid, options: [], range: NSRange( location: 0, length: normalizedDid.count) )
+        
+        guard let textCheckingResult = textCheckingResults?.first else {
             return ""
         }
-
-
-
+        
+        var matches = [String]()
+        for index in 1..<textCheckingResult.numberOfRanges {
+            let range = Range(textCheckingResult.range(at: index), in: normalizedDid )
+            if range != nil {
+                let stringSlice = normalizedDid[ range! ]
+                let match = String( stringSlice  )
+                matches.append( match )
+            }
+        }
+        
+        return matches.first ?? ""
     }
+
 }
