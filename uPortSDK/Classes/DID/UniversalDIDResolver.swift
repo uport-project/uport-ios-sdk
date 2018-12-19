3bad3f8a857f9f64866c328faee8a462e52f6159
Cornelis van der Bent <cornelis@meaning-matters.com>
2018-12-19 15:23:05 +0100

    Add some documentation to universal resolver

--- uPortSDK/Classes/DID/UniversalDIDResolver.swift
+++ uPortSDK/Classes/DID/UniversalDIDResolver.swift
@@ -13,17 +13,35 @@ public enum UniversalDIDResolverError: Error
     case noSuitableResolver
 }
 
+/// A DID resolver that selects the appropriate registered resolver.
 public struct UniversalDIDResolver: DIDResolver
 {
     private var resolvers: [String : DIDResolver] = [ : ]
 
+    /// The queue on which all `completionHandler`s are invoked.  The main queue is the default.
     public private(set) var completionQueue: DispatchQueue
 
+    /// Creates an empty (without resolvers) universal DID resolver.
+    ///
+    /// To make this struct useful, one or more DID resolvers must be installed using `register()`.
+    ///
+    /// - Parameters:
+    ///     - completionQueue: The queue on which all `completionHandler`s are invoked.  The main queue is the default.
+    ///
     init(completionQueue: DispatchQueue = DispatchQueue.main)
     {
         self.completionQueue = completionQueue
     }
 
+    /// Installs a DID resolver.
+    ///
+    /// - Parameters:
+    ///     - resolver: The DID resolver to be installed.
+    ///
+    /// - Throws: `UniversalDIDResolverError.resolverLacksMethod` if the resolver has an empty `method`.
+    ///
+    /// - ToDo: Perform more resolver checks (e.g. checking the validity of the `method` string).
+    ///
     public mutating func register(resolver: DIDResolver) throws
     {
         guard resolver.method.isEmpty == false else
@@ -48,6 +66,17 @@ public struct UniversalDIDResolver: DIDResolver
         }
     }
 
+    /// Resolves the supplied DID asynchronously.
+    ///
+    /// It's safe to call this function on the main thread, because it returns immediately.  The result is received in
+    /// `completionHandler` which is called on the `completionQueue`.  By default `completionQueue` is the main queue.
+    ///
+    /// - Parameters:
+    ///     - did: The DID to be resolved.
+    ///     - completionHandler: Invoked on the `completionQueue` with the result or error.
+    ///     - document: The resolved DID document, or `nil` in case an error occurred.
+    ///     - error: The error, or `nil` if successful.
+    ///
     public func resolveAsync(did: String,
                              completionHandler: @escaping (_ document: DIDDocument?, _ error: Error?) -> Void)
     {
