7d656745f493f47ba7558d3fdb8cca158e98d6ff
Cornelis van der Bent <cornelis@meaning-matters.com>
2019-02-05 00:30:03 +0100

    Export used frameworks

--- UPort/UPort.h
+++ UPort/UPort.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char UPortVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <UPort/PublicHeader.h>
 
-//@import CryptoSwift;
+@import openssl;
+@import CoreEth;
+@import UPTEthereumSigner;
