7a4a80d08bda8b9977e95f8fbe88de268ac26105
Cornelis van der Bent <cornelis@meaning-matters.com>
2019-02-07 21:18:22 +0100

    Delete CocoaPods based example app

--- Example/Pods/Target Support Files/CoreEthereum/CoreEthereum-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CoreEthereum : NSObject
-@end
-@implementation PodsDummy_CoreEthereum
-@end
--- Example/Pods/Target Support Files/UPTEthereumSigner/UPTEthereumSigner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UPTEthereumSigner : NSObject
-@end
-@implementation PodsDummy_UPTEthereumSigner
-@end
